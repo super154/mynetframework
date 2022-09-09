4a3fb026868cd2a15f3e7185995862771249daf0
super154 <1548613410@qq.com>
2022-09-09 06:15:43 +0000

    ThreadPool

--- base/ThreadPool.h
+++ base/ThreadPool.h
@@ -4,6 +4,33 @@
 #include "Mutex.h"
 #include "Condition.h"
 #include "Thread.h"
+#include "Type.h"
+
+namespace base
+{
+class ThreadPool
+{
+public:
+	typedef std::function<void ()> Task;
+
+	explicit ThreadPool(const string& nameArg = string("ThreadPool"));
+	~ThreadPool();
+
+	void setMaxQueueSize(int maxSize) {maxQueueSize_ = maxSize;};
+	void setThreadInitCallbase(const Task& cb) {threadInitCallbase_ = cb};
+	void start(int numThreads);
+	void stop();
+	const string& name() const { return name_};
+	size_t queueSize() const ;
+	void run(Task t);
+private:
+	bool isFull() const;
+	void runInThread();
+	Task task();
+
+	
+}
+}
 
 #include <deque>
 #include <vector>
